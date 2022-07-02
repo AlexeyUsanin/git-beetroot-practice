dc7ca9c7e158f7c641608cebb6e677e011db278f
Alexey Usanin <aleksej.usanin@icloud.com>
2022-07-02 11:34:48 +0300

    Gulp

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -2,6 +2,11 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const browserSync = require('browser-sync').create();
 const concat = require('gulp-concat');
+const del = require('del');
+const zip = require('gulp-zip');
+const imagemin = require('gulp-imagemin');
+const cssnano = require('gulp-cssnano');
+const uglify = require('gulp-uglify');
 
 function liveReload() {
   browserSync.init({
@@ -25,6 +30,38 @@ function jsCompile() {
     .pipe(gulp.dest('./src/js'));
 }
 
+function buildCss() {
+  return gulp.src('./src/css/*.css')
+    .pipe(cssnano())
+    .pipe(gulp.dest('build/css'));
+}
+
+function buildJS() {
+  return gulp.src('./src/js/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('build/js'));
+}
+
+function buildHTML() {
+  return gulp.src('./src/*.html')
+    .pipe(gulp.dest('build'));
+}
+
+function clean() {
+  return del('build');
+}
+
+function archive() {
+  return gulp.src('build/**/*')
+    .pipe(zip('build.zip'))
+    .pipe(gulp.dest('./'));
+}
+
+function optimizeImg() {
+  return gulp.src('./src/img/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('build/img'))
+}
+
 exports.default = liveReload;
-exports.sass = cssTranspile;
-exports.js = jsCompile;
+exports.build = gulp.series(clean, gulp.parallel(buildCss, buildJS, buildHTML, optimizeImg), archive);
